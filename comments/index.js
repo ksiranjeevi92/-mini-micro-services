e6566c7c272eea4e14c35c9498ea35501de6c232
siranjeevi <ksiranjeevi92@gmail.com>
2025-11-23 11:17:08 +0530

    initial commit

---
+++ comments/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  if (commentsByPostId[req.params.id])
+    res.status(200).send(commentsByPostId[req.params.id]);
+  else return res.status(404).send("not found");
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  return res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Server listening in 4001");
+});
